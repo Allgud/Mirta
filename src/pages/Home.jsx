03eca5f2b39f12d8f92010ef8a802a13e5e8f6cb
Allgud <allgud83@gmail.com>
2023-06-08 21:52:33 +0300

    fixed pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Sorting from "../components/Sorting";
 export default function Home() {
   const { postsView, currentPage } = useSelector(store => store.posts)
   const { users } = useSelector(store => store.users)
-  
+
   return (
     <Container style={{ width: "100%" }}>
       <Sorting />
--- src/store/helpers.js
+++ src/store/helpers.js
@@ -1,10 +1,12 @@
+const ITEMS_ON_PAGE = 10
+
 export const createPostPagination = (array) => {
   const start = JSON.parse(JSON.stringify(array))
-  const ITEMS_ON_PAGE = 10
+  const edge = Math.ceil(array.length / ITEMS_ON_PAGE)
   const result = {}
 
-  for (let i = 0; i < ITEMS_ON_PAGE; i++) {
-    result[i + 1] = start.splice(0, 10)
+  for (let i = 0; i < edge; i++) {
+    result[i + 1] = start.splice(0, ITEMS_ON_PAGE)
   }
 
   return result
