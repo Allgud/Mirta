ea76e51794c9931417ae2b2c23dd79aa7967e6e7
Allgud <allgud83@gmail.com>
2023-06-10 12:03:10 +0300

    change sorting dropdown

--- src/components/Sorting/index.jsx
+++ src/components/Sorting/index.jsx
@@ -1,20 +1,45 @@
-import { Container, Dropdown } from "react-bootstrap";
+import { Dropdown, Container } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { sortFromBegin, sortFromEnd, defaultSort } from '../../store/actions/actionCreator'
+import { useState } from "react";
+
+const items = ["Default", "A - Z", "Z - A"]
 
 export default function Sorting() {
   const dispatch = useDispatch()
+  const [mode, setMode] = useState('Default')
+
+  const handleChange = (value) => {
+    setMode(value)
+    switch (value) {
+      case "Default": {
+        dispatch(defaultSort())
+        break
+      }
+      case "A - Z": {
+        dispatch(sortFromBegin())
+        break
+      }
+      case "Z - A": {
+        dispatch(sortFromEnd())
+        break
+      }
+      default: setMode('Default')
+    }
+  }
 
   return (
     <Container className="mt-2 d-flex justify-content-end">
       <Dropdown>
         <Dropdown.Toggle variant="primary" >
-          Sort by
+          Sort by: {mode}
         </Dropdown.Toggle>
         <Dropdown.Menu>
-          <Dropdown.Item as="button" onClick={() => dispatch(defaultSort())}>Default</Dropdown.Item>
-          <Dropdown.Item as="button" onClick={() => dispatch(sortFromBegin())}>A - Z</Dropdown.Item>
-          <Dropdown.Item as="button" onClick={() => dispatch(sortFromEnd())}>Z - A</Dropdown.Item>
+          {
+            items.map(item => (
+              <Dropdown.Item as="button" key={item} value={item} onClick={(evt) => handleChange(evt.target.value)}>{item}</Dropdown.Item>
+            ))
+          }
         </Dropdown.Menu>
       </Dropdown>
     </Container>
